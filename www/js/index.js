73a5401aca7a376b845e1c62a7f1dd62c4a9951a
BoliviaBigby <rigo1_15@uni.worc.ac.uk>
2016-10-27 14:41:39 +0100

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,15 @@
+
+    function onLoad() {
+        document.addEventListener("deviceready", onDeviceReady, false);
+		console.log("device ready"); //here is the event listener 
+    }
+	
+	
+
+    // device APIs are available
+    // here is the event handler
+    function onDeviceReady() {
+		alert("hi olivia"); //the event handler reacts to a function
+        
+	
+    }
