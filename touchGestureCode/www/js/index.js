135956183cbfc0b0922f0403193133213f2fc4d6
BoliviaBigby <rigo1_15@uni.worc.ac.uk>
2016-11-17 14:29:41 +0000

    added in a button with math random

--- touchGestureCode/www/js/index.js
+++ touchGestureCode/www/js/index.js
@@ -2,20 +2,11 @@
 
 $(document).on("pagecreate","#pageone",function(){
   
-  	$('#tapholdtext').on("taphold",function(){
-    	$(this).hide();
- 	});                       
+  	$('#tapholdtext').on("taphold",function random() {
+	return !Math.round(Math.random());
+}
+{
+    	
 
-	$('#taptext').on("tap",function(){
-    	$(this).css('color', 'red');
- 	}); 
-
-	$('#swipetext').on("swipeleft",function(){
-    	$(this).css('color', 'green');
-  	});   
-
-	$('#swipetext').on("swiperight",function(){
-    	$(this).css('color', 'blue');
-  	});   
 
 });
